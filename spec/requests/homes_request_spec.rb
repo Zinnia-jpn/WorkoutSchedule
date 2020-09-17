5d5ce454a3fd5859b4b616cf5b0d068a2f7f99c0
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-09-17 17:17:19 +0900

    Added a test case

--- spec/requests/homes_request_spec.rb
+++ spec/requests/homes_request_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-
-  describe "GET /top" do
-    it "returns http success" do
-      get "/homes/top"
-      expect(response).to have_http_status(:success)
+  describe "#top" do
+    context "正常なHTTPリクエストを送った時" do
+      example "リクエストが成功する" do
+        get :"/"
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
 end
