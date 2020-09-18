d46fe065a157f957249a0168bd3d5c69812695eb
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-09-18 20:35:41 +0900

    Created users table

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+
+  validates: :name, presence: true
+  validates: :name, presence: true
+  validates: :image, presence: true
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    name { "MyString" }
+    mail_address { "MyString" }
+    image { "MyString" }
+  end
+end
