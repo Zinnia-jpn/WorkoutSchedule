f26641c97cb130e8d37086e05af6e45a33c4aec5
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-09-23 15:08:59 +0900

    Added test case for password column

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,11 +3,13 @@ FactoryBot.define do
     name { "Name Sample" }
     email { "mail-sample@rails.com" }
     image { "image.jpeg" }
+    password { "password_sample" }
   end
 
   factory :user_2, class: User do
     name { "Name Sample_2" }
     email { "mail-sample_2@rails.com" }
     image { "image_2.jpeg" }
+    password { "password_sample_2" }
   end
 end
