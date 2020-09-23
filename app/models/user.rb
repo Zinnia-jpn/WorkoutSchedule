83dfe50ca9405eea7ef16602df8ac6e792a58e29
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-09-23 16:07:09 +0900

    Fixed validation of email column in Users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :image, presence: true
   validates :password_digest, presence: true
 end
