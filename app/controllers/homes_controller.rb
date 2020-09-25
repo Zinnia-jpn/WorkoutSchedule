df77714a30ff40bae54e25c4a6f5f0955c310e15
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-09-25 19:59:49 +0900

    Implemented login function

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,4 +4,15 @@ class HomesController < ApplicationController
 
   def login_form
   end
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      redirect_to "/user"
+    else
+      @error = "メールアドレスかパスワードが間違っています"
+      render "login_form"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get "login" => "homes#login_form"
+  post "login" => "homes#login"
   get "user" => "users#show"
   get "/" => "homes#top"
 end
