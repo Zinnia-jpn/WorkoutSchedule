a585c063f524ef654e89f5b9f418a9a497fee8ed
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-09-29 16:44:40 +0900

    Added constraints to the exercise_categories teble

--- db/migrate/20200928052730_create_exercise_categories.rb
+++ db/migrate/20200928052730_create_exercise_categories.rb
@@ -1,9 +1,9 @@
 class CreateExerciseCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :exercise_categories do |t|
-      t.string :name
-      t.integer :muscle_parts_id
-      t.boolean :whether_aerobic
+      t.string :name, null: false
+      t.integer :muscle_parts_id, null: false
+      t.boolean :whether_aerobic, default: false, null: false
 
       t.timestamps
     end
