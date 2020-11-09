afa42a4e1bde8a5f03fc2d5b3555fde60430644f
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-11-09 11:08:44 +0900

    Implement new user registration function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def new
+    @user = User.new()
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password], image: "default_user_image.jpeg")
+    if @user.save
+      flash[:success] = "ユーザー登録が完了しました"
+      redirect_to "/schedule"
+    else
+      render "new"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post "logout" => "sessions#logout"
   post "login" => "sessions#login"
   get "login" => "sessions#login_form"
+  post "user/create" => "users#create"
+  get "signup" => "users#new"
   get "user" => "users#show"
   get "/" => "homes#top"
 end
