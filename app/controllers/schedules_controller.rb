e230b23057bbca1cbdf6b6215d0c72c5601d5be9
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-12-06 22:22:13 +0900

    View workout data for the day

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,8 @@
 class SchedulesController < ApplicationController
   def index
+    @user = User.find_by(id: session[:user_id])
+    @workouts = Workout.all
+    @records = Record.where(user_id: @user.id)
+    @today_records = @records.where(date: Date.today)
   end
 end
