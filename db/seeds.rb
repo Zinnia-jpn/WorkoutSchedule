ce4e011c40c7bbee40379851bd0d286d2e8e3d3a
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-12-06 13:12:45 +0900

    Renamed CSV files

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,19 @@ require "csv"
 # coding: utf-8
 
 CSV.foreach("db/csv/users.csv") do |row|
-  User.create(name: row[0], email: row[1], password: row[2], password_confirmation: row[2], image: row[3])
-end
-
-CSV.foreach("db/csv/exercise_categories.csv") do |row|
-  ExerciseCategory.create(name: row[0], muscle_parts_id: row[1])
-end
-
-CSV.foreach("db/csv/muscle_parts.csv") do |row|
-  MusclePart.create(id: row[0], name: row[1], image: row[2])
+  User.create!(name: row[0], email: row[1], password: row[2], password_confirmation: row[2], image: row[3])
 end
 
 CSV.foreach("db/csv/workouts.csv") do |row|
-  Workout.create(user_id: row[0], date: row[1], whether_plan: row[2], exercise_id: row[3],
-                 muscle_parts_id: row[4], weight: row[5], rep: row[6], set: row[7],
-                 interval: row[8], time: row[9], intensity: row[10], remark: row[11])
+  Workout.create!(id: row[0], name: row[1], category_id: row[2])
+end
+
+CSV.foreach("db/csv/categories.csv") do |row|
+  Category.create!(id: row[0], name: row[1])
+end
+
+CSV.foreach("db/csv/records.csv") do |row|
+  Record.create!(user_id: row[0], date: row[1], plan_flag: row[2], workout_id: row[3],
+                weight: row[4], rep: row[5], set: row[6], interval: row[7], time: row[8],
+                intensity: row[9], remark: row[10])
 end
