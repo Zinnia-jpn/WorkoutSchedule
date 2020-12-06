79da3adda81d7ed86031681ef1519eb66191b410
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-12-07 01:16:35 +0900

    Information browsing function of the day is completed

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -4,5 +4,6 @@ class SchedulesController < ApplicationController
     @workouts = Workout.all
     @records = Record.where(user_id: @user.id)
     @today_records = @records.where(date: Date.today)
+    @intensities = Intensity.all
   end
 end
