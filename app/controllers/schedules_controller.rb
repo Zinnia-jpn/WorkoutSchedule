7e5b11bf1ea592e4b42288dd96d328b28090f4f0
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-12-07 13:47:53 +0900

    Partialize the table and add the next day's information

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,8 +2,10 @@ class SchedulesController < ApplicationController
   def index
     @user = User.find_by(id: session[:user_id])
     @workouts = Workout.all
-    @records = Record.where(user_id: @user.id)
-    @today_records = @records.where(date: Date.today)
     @intensities = Intensity.all
+    current_user_records = Record.where(user_id: @user.id)
+    @today_records = current_user_records.where(date: Date.today)
+    @tomorrow_records = current_user_records.where(date: Date.tomorrow)
   end
+
 end
--- spec/requests/schedules_request_spec.rb
+++ spec/requests/schedules_request_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Schedules", type: :request do
-
-  describe "GET /schedule" do
-    it "returns http success" do
-      get schedule_path
-      expect(response).to have_http_status(:success)
+  describe "#index" do
+    context "正常なHTTPリクエストを送った時" do
+      example "リクエストが成功する" do
+        get schedule_path
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
 end
