ae65e62217350fd3c3627482bc0017d00d06b7b0
Zinnia_jpn <lily@m-sho5-aqua.blue>
2020-12-30 05:43:29 +0900

    Implemented record edit function

--- app/javascript/record.js
+++ app/javascript/record.js
@@ -6,7 +6,13 @@ document.addEventListener("turbolinks:load", function () {
                 $.ajax({
                     url: 'dynamic_select_category',
                     type: 'GET',
-                    data: { category_id: $(this).has('option:selected').val(), flag: $('#plan_flag').val() },
+                    data: {
+                        category_id: $(this).has('option:selected').val(), url: $('#url').val(),
+                        id: $('#id').val(), year: $('#_date_1i').val(), month: $('#_date_2i').val(), day: $('#_date_3i').val(),
+                        plan_flag: $('#plan_flag').val(), time: $('#time').val(), intensity: $('#intensity').val(),
+                        weight: $('#weight').val(), rep: $('#rep').val(), set: $('#set').val(), interval: $('#interval').val(),
+                        remark: $('#remark').val()
+                    },
                     dataType: 'script'
                 });
             });
