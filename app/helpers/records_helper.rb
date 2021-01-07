89858a89df7c648a5a7a8569e54762fcaa7edf7b
Zinnia_jpn <lily@m-sho5-aqua.blue>
2021-01-07 13:25:50 +0900

    Rewrite to the URL before the transition using js

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,2 +1,8 @@
 module RecordsHelper
+  # post先のURLを取得
+  def post_url
+    url = get_previous_path
+    previous_action = url[:action]
+    previous_action == "edit" || action_name == "edit" ? record_update_path : record_create_path
+  end
 end
