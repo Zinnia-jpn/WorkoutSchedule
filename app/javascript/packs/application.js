39bf910ca44890f7221f324bd81a274833160d20
Zinnia_jpn <lily@m-sho5-aqua.blue>
2021-01-07 20:44:30 +0900

    Introduced bootstrap and awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,24 +1,17 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// Bootstrap
+import 'bootstrap';
+import '../stylesheets/application';
+
+// Awesome
+import '@fortawesome/fontawesome-free/js/all'
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-// jQuery導入
 require('jquery')
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-// jsファイルの読み込み
 require ('user.js')
 require ('record.js')
 require ('schedule.js')
