4b14ca18d29387dd2aaed9e4dc80e0b6768aad0a
Zinnia_jpn <lily@m-sho5-aqua.blue>
2021-01-19 06:56:16 +0900

    Fixed small details

--- app/models/record.rb
+++ app/models/record.rb
@@ -15,8 +15,10 @@ class Record < ApplicationRecord
   validates :remark, length: { maximum: 255 }
 
   # 指定された範囲のRecordを取得
-  def get_specified_records(user_id, date_range)
-    get_user_records(user_id).where(date: date_range)
+  def self.get_specified_records(user_id, date_range)
+    # 指定されたユーザーのRecordを全て取得・並び替え
+    user_records = self.where(user_id: user_id).records_sort
+    user_records.where(date: date_range)
   end
 
   private
@@ -28,8 +30,8 @@ class Record < ApplicationRecord
     end
   end
 
-  # 現在のユーザーのRecordを全て取得
-  def get_user_records(user_id)
-    Record.where(user_id: user_id).order(date: "ASC", workout_id: "ASC")
+  # 該当のレコードを並び替える
+  def self.records_sort
+    self.order(date: "ASC", workout_id: "ASC")
   end
 end
